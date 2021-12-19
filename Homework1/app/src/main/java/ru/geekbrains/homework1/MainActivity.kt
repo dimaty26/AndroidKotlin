8cf754dcb58ca62001b34040c4439fbb6d8ca9ef
dimaty26 <dimaty26rus@mail.ru>
2021-12-19 22:09:34 +0300

    Homework1 - Added button and set clickListener

--- Homework1/app/src/main/java/ru/geekbrains/homework1/MainActivity.kt
+++ Homework1/app/src/main/java/ru/geekbrains/homework1/MainActivity.kt
@@ -1,11 +1,16 @@
 package ru.geekbrains.homework1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var button = findViewById<Button>(R.id.button1)
+        button.setOnClickListener { Toast.makeText(this, "Clicked", Toast.LENGTH_LONG).show() }
     }
 }
