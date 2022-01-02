b2c599a0c9dedcf60f061414aa1be9a4952c2855
dimaty26 <dimaty26rus@mail.ru>
2022-01-02 21:35:41 +0300

    Homework4 - merged homework3

---
+++ Homework4/app/src/main/java/ru/geekbrains/homework3/MainActivity.kt
@@ -0,0 +1,23 @@
+package ru.geekbrains.homework3
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.geekbrains.homework3.databinding.ActivityMainBinding
+import ru.geekbrains.homework3.fragments.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance()).commit()
+        }
+    }
+}
---
+++ Homework4/app/src/main/java/ru/geekbrains/homework3/MainViewModel.kt
@@ -0,0 +1,28 @@
+package ru.geekbrains.homework3
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.lang.Thread.sleep
+import java.util.*
+
+class MainViewModel(
+    private val liveData: MutableLiveData<AppState> = MutableLiveData(),
+    private val random: Random = Random(),
+    private var list: List<AppState> = arrayListOf(
+        AppState.Success("OK"),
+        AppState.Loading(50), AppState.Error(Exception("Just Error"))
+    )
+) : ViewModel() {
+
+    fun getLiveData(): LiveData<AppState> {
+        return liveData
+    }
+
+    fun getDataFromServer() {
+        Thread {
+            sleep(5000)
+            liveData.postValue(list[random.nextInt(3)])
+        }.start()
+    }
+}
