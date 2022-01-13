f66480dd88484d42f28ea5308ec134417641b9bc
dimaty26 <dimaty26rus@mail.ru>
2022-01-13 21:23:59 +0300

    Homework4 - Optimize packages

--- Homework1/app/src/main/java/ru/geekbrains/homework1/Car.kt
+++
@@ -1,9 +0,0 @@
-package ru.geekbrains.homework1
-
-object Car {
-    private const val model: String = "Tesla"
-
-    fun getModel(): String {
-        return model
-    }
-}
--- Homework1/app/src/main/java/ru/geekbrains/homework1/Weather.kt
+++
@@ -1,3 +0,0 @@
-package ru.geekbrains.homework1
-
-data class Weather(var city: String, var temper: Int)
