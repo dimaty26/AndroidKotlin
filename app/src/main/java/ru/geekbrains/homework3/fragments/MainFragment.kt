eb6abf7f7c187dab9318f39f8db39cfd8462cbf7
dimaty26 <dimaty26rus@mail.ru>
2022-01-16 19:35:24 +0300

    Homework5 - Created Internet connection. Model Updated

--- app/src/main/java/ru/geekbrains/homework3/fragments/MainFragment.kt
+++ app/src/main/java/ru/geekbrains/homework3/fragments/MainFragment.kt
@@ -6,10 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.google.android.material.snackbar.Snackbar
+import com.bumptech.glide.Glide
+import ru.geekbrains.homework3.api.POSTER_BASE_URL
+import ru.geekbrains.homework3.api.TheMovieDBClient
+import ru.geekbrains.homework3.api.TheMovieDBService
 import ru.geekbrains.homework3.databinding.FragmentMainBinding
-import ru.geekbrains.homework3.model.Movie
+import ru.geekbrains.homework3.model.MovieDetails
+import ru.geekbrains.homework3.model.Repository
+import ru.geekbrains.homework3.model.RepositoryImpl
 import ru.geekbrains.homework3.viewmodel.AppState
 import ru.geekbrains.homework3.viewmodel.MainViewModel
 
@@ -18,43 +24,44 @@ class MainFragment : Fragment() {
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
     private lateinit var viewModel: MainViewModel
+    private lateinit var repository: Repository
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        viewModel.getLiveData().observe(viewLifecycleOwner, Observer<AppState> { renderData(it) })
-        viewModel.getDataFromServer()
+
+        val apiService: TheMovieDBService = TheMovieDBClient.getClient()
+        repository = RepositoryImpl(apiService)
+
+        viewModel = getViewModel(1) //id = 1 is example
+        viewModel.movieDetails.observe(viewLifecycleOwner, Observer { renderData(it) })
+        viewModel.appState.observe(viewLifecycleOwner, Observer {
+            binding.progressBar.visibility = if (it == AppState.Loading) View.VISIBLE else View.GONE
+            binding.txtError.visibility =
+                if (it == AppState.Error(Throwable())) View.VISIBLE else View.GONE
+        })
     }
 
-    private fun renderData(appState: AppState) {
-        when (appState) {
-            is AppState.Loading -> {
-                binding.loadingLayout.visibility = View.VISIBLE
+    private fun getViewModel(movieId: Int): MainViewModel {
+        return ViewModelProvider(this, object : ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                return MainViewModel(repository, movieId) as T
             }
-            is AppState.Success -> {
-                val movie = appState.movieResponse
-                binding.loadingLayout.visibility = View.GONE
-                setData(movie)
-            }
-            is AppState.Error -> {
-                val data = appState.error.message
-                binding.loadingLayout.visibility = View.GONE
-                if (data != null) {
-                    Snackbar
-                        .make(binding.mainView, data, Snackbar.LENGTH_INDEFINITE)
-                        .setAction("Reload") { viewModel.getDataFromServer() }
-                        .show()
-                }
-            }
-        }
+        })[MainViewModel::class.java]
     }
 
-    private fun setData(movie: Movie) {
-        binding.movieName.text = movie.name
-        binding.description.text = movie.description
-        binding.year.text = movie.year.toString()
-        binding.movieLength.text = movie.length.toString()
+    private fun renderData(movie: MovieDetails) {
+        setData(movie)
+    }
+
+    private fun setData(movie: MovieDetails) {
+        binding.movieName.text = movie.title
+        binding.description.text = movie.overview
+        binding.year.text = movie.releaseDate
+        binding.movieLength.text = movie.runtime.toString() + " minutes"
         binding.ratings.text = movie.rating.toString()
+
+        val moviePosterURL = POSTER_BASE_URL + movie.posterPath
+        Glide.with(this).load(moviePosterURL).into(binding.moviePoster)
     }
 
     override fun onCreateView(
--- app/src/main/java/ru/geekbrains/homework3/model/Movie.kt
+++ app/src/main/java/ru/geekbrains/homework3/model/Movie.kt
@@ -9,11 +9,37 @@ data class Movie(
     val rating: Double
 )
 
-fun getDefaultMovie() = Movie(
-    DefaultMovie.name,
-    DefaultMovie.type,
-    DefaultMovie.description,
-    DefaultMovie.year,
-    DefaultMovie.length,
-    DefaultMovie.rating
+fun getDefaultMovies() = listOf(
+    Movie(
+        "Spider-Man: No Way Home",
+        "comics",
+        "Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero.",
+        2021,
+        155,
+        8.7
+    ),
+    Movie(
+        "Eternals",
+        "comics",
+        "The Eternals are a team of ancient aliens who have been living on Earth in secret for thousands of years.",
+        2021,
+        143,
+        6.9
+    ),
+    Movie(
+        "Encanto",
+        "cartoon",
+        "The tale of an extraordinary family, the Madrigals, who live hidden in the mountains of Colombia, in a magical house, in a vibrant town, in a wondrous, charmed place called an Encanto.",
+        2021,
+        132,
+        8.6
+    ),
+    Movie(
+        "Resident Evil: Welcome to Raccoon City",
+        "thriller",
+        "Once the booming home of pharmaceutical giant Umbrella Corporation, Raccoon City is now a dying Midwestern town.",
+        2021,
+        144,
+        6.9
+    )
 )
